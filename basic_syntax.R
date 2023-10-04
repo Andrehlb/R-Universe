d9ce536925f099c926daaefe602378cf6d5fc2f6
Andrehlb <andrehlb@me.com>
2023-10-03 22:45:33 -0300

    Updated version of the basic_syntax.R file

---
+++ basic_syntax.R
@@ -0,0 +1,16 @@
+# This is a basic R script file displayed to demonstrate the basic syntax of R.
+
+# Variables are assigned using the assignment operator '<-' or '='
+
+x <- 10
+name <- "André"
+print(paste("The x value is:", x))
+print(paste("My name is:", name))
+
+# Arithmetic operators are the same as in other languages
+
+sum <- x + 2
+print(paste("The sum of x and 2 is:", sum))
+
+# Conditional statements are the same as in other languages
+
