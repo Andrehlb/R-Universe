e82955dfdec8eb280cd409d4c787729e2de8d773
Andrehlb <andrehlb@me.com>
2023-10-03 22:58:17 -0300

    Updated version of the basic_syntax.R file

--- basic_syntax.R
+++ basic_syntax.R
@@ -14,3 +14,31 @@ print(paste("The sum of x and 2 is:", sum))
 
 # Conditional statements are the same as in other languages
 
+if (x > 5) {
+    print("x is grather than 5")
+} else {
+    print("X is less than 5")
+}
+
+# Loops are the same as in other languages, using the 'for' and 'while' keywords
+
+print("Loop with for: ")
+for (i in 1:5) {
+    print(i)
+}
+
+print ("Loop with while: ")
+i <- 1
+while (i <= 5) {
+    print(i)
+    i <- i + 1
+}
+
+# Functions are defined using the 'function' keyword
+
+my_function <- function(a, b) {
+    return (a + b)
+}
+
+result <- my_function(3, 4)
+print(paste("The result of the function is: ", result))
